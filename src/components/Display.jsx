45498a131a197d6000fb6435467dbbb9e9b34e30
dinbusan <deann.videtto@gmail.com>
2023-06-07 23:12:36 +0200

    get request is working

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import axios from 'axios'
+
+axios.defaults.headers.common["token"] = "pj11daaQRz7zUIH56B9Z";
+
 
 const Display = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          'https://frontend-case-api.sbdev.nl/api/posts?page=1&perPage=10&sortBy=title&sortDirection=asc&searchPhrase=test ber&categoryId=1'
+        );
+        setData(response.data.data);
+      } catch (error){
+        console.log('An error occurred:', error);
+      }
+    };
+    fetchData();
+  }, [])
+console.log(data);
   return (
-    <div>Display</div>
-  )
+    <div>
+      <h2>Display Component</h2>
+      <ul>
+        {data.map((item) => (
+          <li key={item.id}>{item.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default Display
